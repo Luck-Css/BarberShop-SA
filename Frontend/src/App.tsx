2b5c684f20aa3e16a0b5cb9cab67731051e8e0ab
Luck-Css <lucascharff@proton.me>
2026-08-19 09:32:26 -0300

    chore: Initial project folders creation

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="flex justify-center text-5xl text-amber-400">Testes Osss</h1>
+    </>
+  )
+}
+
+export default App
---
+++ LandingPage/src/App.tsx
@@ -0,0 +1,21 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <header>
+          <nav className="flex justify-between">
+            <h1>Pet <label htmlFor="" className="text-amber-400">Care</label></h1>
+            <button>Inicio</button>
+            <button>Funcionalidades</button>
+            <button>Contato</button>
+            <button>Agendar Consulta</button>
+          </nav>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
